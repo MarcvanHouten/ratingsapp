81eee30de5ed1f20179744715e80dd7ea2720ef3
Nicolas <aplusdepseudo@gmail.com>
2021-11-16 16:52:44 +0100

    Added ComosDB binding to Ratings API

--- Ratings/ratings.py
+++ Ratings/ratings.py
@@ -3,22 +3,11 @@ import logging
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if not ratings:
+        logging.warning("No rating found")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        # Return the ratings in JSON format
+        return func.HttpResponse(f"", status_code=200)
